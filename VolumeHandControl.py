2d020815d38501e4b42ef49c0f151a2baab13b34
Pallab Sarangi <pallab2902@gmail.com>
2024-04-09 19:55:43 +0530

    Update VolumeHandControl.py

--- VolumeHandControl.py
+++ VolumeHandControl.py
@@ -1,11 +1,12 @@
 import cv2
+import numpy as np
 import mediapipe as mp
 import time
-import numpy as np
-import HandTrackingModule as htm
 import math
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+import requests
+import HandTrackingModule as htm
 
 wCam, hCam = 640, 480
 
@@ -14,49 +15,50 @@ interface = devices.Activate(
     IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
 volume = interface.QueryInterface(IAudioEndpointVolume)
 
-
 volRange = volume.GetVolumeRange()
-minVol= volRange[0]
-maxVol= volRange[1]
-
-# Hand range 50 -300
-# Volume Range -65 - 0
-
-
-
+minVol = volRange[0]
+maxVol = volRange[1]
 
 pTime = 0
 cTime = 0
-cap = cv2.VideoCapture(0)
-cap.set(3, wCam)
-cap.set(4, hCam)
+
+url = "http://192.168.0.103:8080/shot.jpg"  
+
 detector = htm.handDetector(detectionCon=0.7)
 
 volBar = 400
-volPer = 0 
+volPer = 0
 
 while True:
-    success, img= cap.read()
-    if not success:
-        break
-    img = detector.findHands(img)
-    lmList= detector.findPosition(img, draw=False)
-    if len(lmList) != 0:
+    img_resp = requests.get(url)
+    img_arr = np.array(bytearray(img_resp.content), dtype=np.uint8)
+    img = cv2.imdecode(img_arr, -1)
 
-        x1, y1= lmList[4][1], lmList[4][2]
-        x2, y2= lmList[8][1], lmList[8][2]
-        cx, cy = (x1 + x2) // 2, (y1+y2)//2
+    if img is None:
+        break
+
+    img = cv2.resize(img, (wCam, hCam))
+
+    img = cv2.flip(img, 1)
+
+    img = detector.findHands(img)
+    lmList = detector.findPosition(img, draw=False)
+    
+    if lmList:
+        x1, y1 = lmList[4][1], lmList[4][2]
+        x2, y2 = lmList[8][1], lmList[8][2]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
 
         cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
         cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
-        cv2.line(img, (x1,y1), (x2, y2), (255, 0, 255), 3)
+        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
 
-        length = math.hypot(x2-x1, y2-y1)
+        length = math.hypot(x2 - x1, y2 - y1)
 
-        if length<50:
+        if length < 50:
             cv2.circle(img, (cx, cy), 15, (255, 255, 0), cv2.FILLED)
-        
-        if length==300:
+
+        if length == 300:
             cv2.circle(img, (x1, y1), 15, (255, 255, 0), cv2.FILLED)
             cv2.circle(img, (x2, y2), 15, (255, 255, 0), cv2.FILLED)
 
@@ -64,17 +66,19 @@ while True:
         volBar = np.interp(length, [50, 300], [400, 150])
         volPer = np.interp(length, [50, 300], [0, 100])
 
-
         cv2.rectangle(img, (50, 150), (85, 400), (255, 0, 0), 3)
         cv2.rectangle(img, (50, int(volBar)), (85, 400), (255, 0, 0), cv2.FILLED)
-        cv2.putText(img, f'{int(volPer)} %', (40, 450), cv2.FONT_HERSHEY_COMPLEX,1, (255, 0, 0), 3)
+        cv2.putText(img, f'{int(volPer)} %', (40, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 3)
         volume.SetMasterVolumeLevel(vol, None)
-    
-    cTime = time.time()
-    fps = 1/(cTime-pTime)
+
+    '''  cTime = time.time()
+    fps = 1 / (cTime - pTime)
     pTime = cTime
 
-    cv2.putText(img, f'FPS:{int(fps)}', (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)    
+    cv2.putText(img, f'FPS:{int(fps)}', (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)'''
     cv2.imshow("Image", img)
-    if cv2.waitKey(1)== 27:
+    
+    if cv2.waitKey(1) == 27:
         break
+
+cv2.destroyAllWindows()
